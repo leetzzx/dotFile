09c135bdb2cebf3b0598c963878f4b23e1d9b06e
leetz <ltzzx12138@gmail.com>
2021-06-04 08:56:09 +0800

    emacs added

---
+++ emacs/lisp/init-ui.el
@@ -0,0 +1,9 @@
+(add-to-list 'default-frame-alist
+             '(font . "Source Code Pro-14"))
+;;(setq initial-frame-alist (quote ((fullscreen . maximized))))
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(setq inhibit-splash-screen t)
+;; (global-hl-line-mode t)
+(provide 'init-ui)
